1d129c32a67f8d55335b12d6e9f1faea145259d5
HikaruSuna <sunkun2525@gmail.com>
2026-09-22 14:24:19 +0900

    chore: Firebase Web設定を追加

--- static/firebase-config.js
+++ static/firebase-config.js
@@ -1,8 +1,8 @@
 // Firebase ConsoleでWebアプリを登録した後、この4項目を設定します。
 // Web用のFirebase設定値は公開される前提です。DBの保護はFirestore Rulesで行います。
 window.FIREBASE_CONFIG = Object.freeze({
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  appId: '',
+  apiKey: 'AIzaSyBpUo1JX-ooto7pZu-Cfwg70QxRmHMr1g4',
+  authDomain: 'nepp-sukejuru-f8c30.firebaseapp.com',
+  projectId: 'nepp-sukejuru-f8c30',
+  appId: '1:1089043998006:web:6f7ee37dbf0e07ccfeff97',
 });
